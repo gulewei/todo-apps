6b787ce9d621448ef34741ef96097ff7f5b7b0d1
yuanzf <yuanzf@114995.com>
2017-12-05 17:00:09 +0800

    modify create app

--- src/hyperTodoRedux/index.js
+++ src/hyperTodoRedux/index.js
@@ -1,15 +1,21 @@
+import { app } from "hyperapp"
 import MainView from './components/MainView'
-import todoApp from './reducers'
 import { createStore } from 'redux'
-import createReduxApp from './hyperapp-redux'
+import todoApp from './reducers'
+import { provider } from './hyperapp-redux'
 
 /**
  * note that hyperapp has a diffrent concept of actions with redux
  * actions in hyperapp are same like reducers in redux
  */
 
-createReduxApp(
-  createStore(todoApp),
-  MainView,
+let store = createStore(todoApp)
+
+// mount app
+const { reDraw } = app(
+  provider(store, MainView),
   document.getElementById('root')
 )
+
+// update state
+store.subscribe(reDraw)
